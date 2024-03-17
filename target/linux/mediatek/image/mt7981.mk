b67d5f77bd21c0c472976b03de2c01bf519c2167
Lynn-Becky <60915579+Lynn-Becky@users.noreply.github.com>
2024-03-17 22:13:37 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -563,7 +563,7 @@ define Device/nokia_ea0326gmp
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  IMAGE_SIZE := 116736k
+  IMAGE_SIZE := 5789k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
