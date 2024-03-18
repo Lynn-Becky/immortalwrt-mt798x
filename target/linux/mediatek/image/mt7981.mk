4c09f5585eab22127794fc31b526cb75cac0d8b1
Lynn-Becky <60915579+Lynn-Becky@users.noreply.github.com>
2024-03-19 01:05:08 +0800

    modified:   target/linux/mediatek/image/mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -553,12 +553,12 @@ define Device/nradio_wt9103_512m
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += nradio_wt9103_512m
+
 define Device/nokia_ea0326gmp
   DEVICE_VENDOR := NOKIA
   DEVICE_MODEL := EA0326GMP
   DEVICE_DTS := mt7981-nokia-ea0326gmp
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
   SUPPORTED_DEVICES := nokia,ea0326gmp
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
